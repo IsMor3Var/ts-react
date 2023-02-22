37e403d41a6872ddce4fe2e12bfdcc030f981959
IsMor3Var <lmoreno9943@gmail.com>
2023-02-21 21:12:32 -0500

    Reducer edit

--- calculator/src/components/Calculator/Panel/Panel.tsx
+++ calculator/src/components/Calculator/Panel/Panel.tsx
@@ -5,10 +5,8 @@ const Panel: FC = () => {
   const { calculator } = useCustomSelector((state) => state);
   return (
     <div>
-      <h2>All: {calculator.valueScreen}</h2>
-      <h3>Operation: {calculator?.operation}</h3>
-      <h3>Value Now: {calculator?.valueNow}</h3>
-      <h3>Value Before: {calculator?.valueBefore}</h3>
+      <h2>{calculator.valueScreen}</h2>
+      <h3>{calculator?.valueNow}</h3>
     </div>
   );
 };
--- calculator/src/redux/slices/calculator/index.ts
+++ calculator/src/redux/slices/calculator/index.ts
@@ -2,17 +2,15 @@ import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 import { CalculateOperation } from 'utils/operations';
 interface CalculatorState {
   valueScreen: string;
-  valueBefore: number | null;
-  valueNow: number | null;
-  operation: string | null;
+  valueNow: number;
+  operation: string;
   history: string[];
 }
 
 const initialState: CalculatorState = {
   valueScreen: '0',
-  valueBefore: null,
-  valueNow: null,
-  operation: null,
+  valueNow: 0,
+  operation: '',
   history: []
 };
 
@@ -24,59 +22,27 @@ const calculatorSlice = createSlice({
       state.valueScreen === '0'
         ? (state.valueScreen = action.payload.trim())
         : (state.valueScreen = `${state.valueScreen} ${action.payload.trim()}`);
-      state.valueNow = Number(action.payload);
+
+      state.valueNow !== 0 && state.operation !== ''
+        ? (state.valueNow = CalculateOperation({
+            param1: Number(state.valueNow),
+            operation: state.operation,
+            param2: Number(action.payload)
+          }))
+        : (state.valueNow = Number(action.payload));
     },
     addOperation: (state, action: PayloadAction<string>) => {
-      const result = CalculateOperation({
-        param1: 2,
-        operation: '+',
-        param2: 2
-      });
-
-      console.log('[ MSG ]', result);
-
       state.valueScreen = `${state.valueScreen} ${action.payload.trim()}`;
       state.operation = action.payload;
-      state.history.push(`${state.valueScreen} ${action.payload}`);
     },
     calculateResult: (state, _) => {
-      console.log('[ MSG ]', state.valueScreen.split('x'));
-
-      // if (
-      //   state.operation !== null &&
-      //   state.valueNow !== null &&
-      //   state.valueScreen !== ''
-      // ) {
-      //   const newValue = parseFloat(state.valueScreen);
-      //   console.log('[ newValue ]', newValue);
-      //   let result: number;
-      //   switch (state.operation) {
-      //     case '+':
-      //       result = state.valueNow + newValue;
-      //       break;
-      //     case '-':
-      //       result = state.valueNow - newValue;
-      //       break;
-      //     case 'x':
-      //       result = state.valueNow * newValue;
-      //       break;
-      //     case '÷':
-      //       result = state.valueNow / newValue;
-      //       break;
-      //     default:
-      //       result = state.valueNow;
-      //       break;
-      //   }
-      //   state.history.push(`${state.valueScreen} = ${result}`);
-      //   state.valueNow = result;
-      //   state.valueScreen = result.toString();
-      //   state.operation = null;
-      // }
+      state.history.push(`${state.valueScreen} = ${state.valueNow}`);
+      state.operation = '';
     },
     cleanScreen: (state, _) => {
-      // state.valueScreen = '0';
-      // state.valueNow = null;
-      // state.operation = null;
+      state.valueScreen = '0';
+      state.valueNow = 0;
+      state.operation = '';
     }
   }
 });
